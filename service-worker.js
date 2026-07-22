4b2ae6aeadf24956beb2875831dc2e1ea0994535
hhakanguner-droid <hhakanguner@gmail.com>
2026-07-22 15:01:43 +0300

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "arven-pt-v4";
+const APP_FILES = ["./", "./index.html", "./manifest.webmanifest"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        if (new URL(event.request.url).origin === self.location.origin) {
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(response => response || caches.match("./index.html")))
+  );
+});
+
